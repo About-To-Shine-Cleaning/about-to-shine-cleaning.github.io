4c7b29186c5ce5d4fbbcaede7eddc8a9ba86c338
About-To-Shine-Cleaning <abouttoshinecleaning.atsc@gmail.com>
2026-02-05 14:37:23 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,13 @@ document.querySelectorAll('.text-btn').forEach(btn => {
     btn.parentElement.classList.toggle('open');
   });
 });
+function handleTextClick() {
+  const guided = confirm("Would you like a guided quote text template?");
+
+  if (guided) {
+    window.location.href =
+      "sms:14842238496?body=Hello%20I%20would%20like%20a%20free%20quote!%0A%0AFloors%3A%0ABedrooms%3A%0ABathrooms%3A%0APets%3F%3A%0ACity%3A";
+  } else {
+    window.location.href = "sms:14842238496";
+  }
+}
