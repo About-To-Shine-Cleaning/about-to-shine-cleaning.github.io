6359e8d869dc198ba1606147d79e387371ddfc0b
About-To-Shine-Cleaning <abouttoshinecleaning.atsc@gmail.com>
2026-02-05 19:28:17 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,19 +7,14 @@ document.addEventListener("DOMContentLoaded", () => {
       nav.classList.toggle("open");
     });
   }
-});
-document.querySelectorAll('.text-btn').forEach(btn => {
-  btn.addEventListener('click', () => {
-    btn.parentElement.classList.toggle('open');
-  });
-});
-function handleTextClick() {
-  const guided = confirm("Would you like a guided quote text template?");
 
-  if (guided) {
-    window.location.href =
-      "sms:14842238496?body=Hello%20I%20would%20like%20a%20free%20quote!%0A%0AFloors%3A%0ABedrooms%3A%0ABathrooms%3A%0APets%3F%3A%0ACity%3A";
-  } else {
-    window.location.href = "sms:14842238496";
-  }
-}
+  window.handleTextClick = function () {
+    const guided = confirm("Would you like a guided quote text template?");
+    if (guided) {
+      window.location.href =
+        "sms:14842238496?body=Hello%20I%20like%20what%20I%20seen%20on%20the%20About%20To%20Shine%20Cleaning%20Website%20and%20would%20like%20a%20free%20quote!%0A%0AFloors%3A%0ABedrooms%3A%0ABathrooms%3A%0APets%3F%3A%0ACity%3A";
+    } else {
+      window.location.href = "sms:14842238496";
+    }
+  };
+});
